6b4192145695e0611406008b2f8ffa77d7ccd403
Joey Jiron <joeyjiron06@gmail.com>
2017-05-08 18:09:10 -0700

    added error messages for auth

--- src/utils/error-messages.js
+++ src/utils/error-messages.js
@@ -5,7 +5,10 @@ module.exports = {
   INVALID_ID : 'You must supply a valid id',
   INVALID_URL : 'You must supply a valid url',
   INVALID_TITLE :  'You must supply a valid title',
+  INVALID_EMAIL: 'You must supply a valid email',
+  INVALID_PASSWORD : 'You must supply a valid password',
   URL_TAKEN : 'Url is already taken',
   FEED_HTTP_ERROR : 'Error fetching your feed',
-  NOT_A_SAVED_FEED_ID : 'User does not have that id saved in the list'
+  NOT_A_SAVED_FEED_ID : 'User does not have that id saved in the list',
+  USER_NOT_EXISTS :  'User does not exist',
 };
